036f81f7eb234bbe6d59ed2f1c7805f73f86f11c
esdncalingo <esdncalingo@gmail.com>
2023-03-31 04:24:46 +0800

    configure redis for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,11 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Use a different cache store in production.
+  config.cache_store = :redis_cache_store, {
+    url: ENV['REDIS_URL']
+  }
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
